17bffd71222395db44b73c886309f37a1a1279ad
Jędrzej Bartoszewicz <jbartoszewicz@sigma.inf.ug.edu.pl>
2012-12-04 09:23:00 +0100

    to jest pierwsza wersja

---
+++ lab6.sh
@@ -0,0 +1,18 @@
+  GNU nano 2.2.6             Plik: lab.txt                                      
+
+#1.
+grep '.' plik.txt
+#2.
+grep '^[0-9]' pl*
+#3.
+grep '^........r' *
+#4.
+grep bash /etc/passwd -c
+#5.
+grep '(X|D|C|M|V|L|I){1,}' plik.txt
+
+
+
+
+
+
